d1147330909afe48130951ed42df4b6c92a4e77a
ablcy <ablcy@users.noreply.github.com>
2026-05-19 16:31:33 +0800

    fix: 修复版本号显示为unknown的问题

--- version.js
+++ version.js
@@ -1,8 +1,8 @@
 const VERSION = {
     major: 5,
     minor: 9,
-    patch: 51,
-    build: '202605191100',
+    patch: 52,
+    build: '202605191200',
     full: function() {
         return `v${this.major}.${this.minor}.${this.patch}`;
     },
